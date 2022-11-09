d0f645a86be380258cb4946b8a1fbec6c30c4905
Sergey <ser.klionoff@yandex.ru>
2022-11-09 16:05:06 +0300

    Заполнен conftest. Добалены фикстуры для дальнейшего тестирования.

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,7 +12,7 @@ import alembic
 from alembic.config import Config
 
 
-#Применяем миграции в начале и в конце тестирования. Upgrade и Downgrade соответственно.
+# Применяем миграции в начале и в конце тестирования. Upgrade и Downgrade соответственно.
 @pytest.fixture(scope="session")
 def apply_migrations():
     warnings.filterwarnings("ignore", category=DeprecationWarning)
@@ -24,10 +24,31 @@ def apply_migrations():
     alembic.command.downgrade(config, "base")
 
 
+# Создаем новый экземпляр приложения для тестов
 @pytest.fixture
 def app(apply_migrations: None) -> FastAPI:
     from app.api.server import get_appliction
 
     return get_appliction()
 
-    
+
+# Ссылка на DB
+@pytest.fixture
+async def db(app: FastAPI) -> Database:
+    return app.state._db
+
+
+# Создаем запросы 
+@pytest.fixture
+async def client(app: FastAPI) -> AsyncClient:
+    async with AsyncClient(
+        app=app,
+        base_url="http://testserver",
+        headers={"Content-Type": "application/json"}
+    ) as client:
+        yield client
+
+
+
+
+
